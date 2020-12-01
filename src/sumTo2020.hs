449b040c1ae450a37dcfe8b28575985ae95738d9
chroth7 <chroth@me.com>
2020-12-01 14:18:33 +0100

    Implement sumTo2020.

--- src/sumTo2020.hs
+++ src/sumTo2020.hs
@@ -1,6 +1,21 @@
 module SumTo2020 where
 
+import Data.List
+
+target :: Int
+target = 2020
+
 sumTo2020 :: [Int] -> (Int, Int)
-sumTo2020 xs = (length xs, length xs)
+sumTo2020 xs = sortedAlgo sorted $ reverse sorted
+  where sorted = sort xs
+
+sortedAlgo :: [Int] -> [Int] -> (Int, Int)
+sortedAlgo [] _ = (0, 0)
+sortedAlgo _ [] = (0, 0)
+sortedAlgo (x:xs) (y:ys)
+  | x + y == target = (x,y)
+  | x + y < target  = sortedAlgo xs (y: ys)
+  | x + y > target  = sortedAlgo (x: xs) ys
+
 
 
