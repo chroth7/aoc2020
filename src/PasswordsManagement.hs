2786bd9d1a2e220b23ccdfc1034176a3c3865741
chroth7 <chroth@me.com>
2020-12-02 11:21:10 +0100

    Clean up.

--- src/PasswordsManagement.hs
+++ src/PasswordsManagement.hs
@@ -12,13 +12,11 @@ parseRule :: String -> PasswordRule
 parseRule input = let packedInput = T.pack input
   in 
     let (min:restMin) = T.splitOn (T.pack "-") packedInput
-      in 
-        let (max:restMax) = T.splitOn (T.pack " ") (head restMin)
-          in 
-            let [letter, password] = restMax
-              in PasswordRule 
-                { minCount=(read $ T.unpack min) :: Int
-                , maxCount=(read $ T.unpack max) :: Int
-                , letter=init $ T.unpack letter
-                , password=T.unpack password}
+        (max:restMax) = T.splitOn (T.pack " ") (head restMin)
+        [letter, password] = restMax
+    in PasswordRule 
+      { minCount=(read $ T.unpack min) :: Int
+      , maxCount=(read $ T.unpack max) :: Int
+      , letter=init $ T.unpack letter
+      , password=T.unpack password}
 
