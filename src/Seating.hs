e10c841732d2f5ad8b173a249a7b19b84c89b395
chroth7 <chroth@me.com>
2020-12-13 15:59:13 +0100

    Day11 WIP.

--- src/Seating.hs
+++ src/Seating.hs
@@ -1,13 +1,43 @@
 module Seating
   ( SeatStatus(..)
-  , SeatingLayout(..)
+  , Layout(..)
+  , parseDay11
+  , changeStatus
+  , countOccAroundSeat
   ) where
+
+import           Data.List
+import           Data.Maybe
+
 
 data SeatStatus = NoSeat | Free | Occupied deriving (Eq, Show)
 
-type Seat = (Int, Int)
+type Column = Int
+type Row = Int
+type SeatCoordinates = (Column, Row)
+type Seat = (SeatCoordinates, SeatStatus)
 
-type SeatingLayout = [[SeatStatus]]
+type Rows = Int
+type Columns = Int
+type SeatingPlan = [Seat]
+data Layout = Layout Rows Columns SeatingPlan deriving (Eq, Show)
+
+parseDay11 :: String -> Layout
+parseDay11 str = Layout numberRows numberCols $ concat $ map (\(colIdx, row) -> parseRow colIdx row) numberedRows
+  where rows = lines str
+        numberRows = length rows
+        numberCols = length $ head rows
+        numberedRows = zip [0..] rows
+
+parseRow :: Int -> String -> [Seat]
+parseRow colIdx row = map parseSeat numberedRow
+  where parseSeat (rowIdx, char) = ((colIdx, rowIdx), parseChar char)
+        numberedRow = zip [0..] row
+
+parseChar :: Char -> SeatStatus
+parseChar 'L' = Free
+parseChar '#' = Occupied
+parseChar _   = NoSeat
 
 changeStatus :: SeatStatus -> Int -> SeatStatus
 changeStatus NoSeat _ = NoSeat
@@ -18,13 +48,14 @@ changeStatus Occupied occ
   | occ > 3 = Free
   | otherwise = Occupied
 
-countOccAroundSeat :: SeatingLayout -> Seat -> Int
-countOccAroundSeat layout (x, y) = sum $ map (checkSeatState layout) seatsAround
+countOccAroundSeat :: Layout -> SeatCoordinates -> Int
+countOccAroundSeat (Layout rows columns layout) (col, row) = sum $ map (isOcc layout) seatsAround
   where
-    columns = length $ head layout
-    rows = length layout
-    seatsAround = [(xx, yy) | xx <- [x-1..x+1], yy <- [y-1..y+1], xx /= yy, xx >= 0, yy >= 0, xx < columns, yy < rows]
+    seatsAround = [(x, y) | x <- [col-1..col+1], y <- [row-1..row+1], x /= y, x >= 0, y >= 0, x < columns, y < rows]
 
-checkSeatState :: SeatingLayout -> Seat -> Int
-checkSeatState layout (x, y) = if seatState == Occupied then 1 else 0
-  where seatState = (layout !! y) !! x
+isOcc :: SeatingPlan -> SeatCoordinates -> Int
+isOcc plan coord = if state == Occupied then 1 else 0
+  where (_, state) = fromJust $ getSeat coord plan
+
+getSeat :: SeatCoordinates -> SeatingPlan -> Maybe Seat
+getSeat coord = find (\(coo, _) -> coo == coord)
